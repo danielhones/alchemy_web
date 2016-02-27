44c04c124dd9f20016110ee2db342f8ac0c9b94b
Daniel Hones <danielhones@gmail.com>
2016-02-27 02:29:25 -0500

    Skeleton HTML, added htmlmin to Gruntfile, removed build/ from tracking

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,22 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+	htmlmin: {
+	    build: {
+		options: {
+		    removeComments: true,
+		    collapseWhitespace: true
+		},
+		files: {
+		    'build/index.html': 'src/index.html'
+		}
+	    }
+	},
 	concat: {
 	    get_comments: {
 		src: ['src/js/alchemy.js'],
 		dest: 'src/concat/alchemy.js',
 	    },
 	},
-
 	uglify: {
 	    my_target: {
 		files: {
@@ -33,10 +43,12 @@ module.exports = function(grunt) {
 	},
     });
 
-    grunt.registerTask('build', ['uglify', 'cssmin']);
+    grunt.registerTask('build', ['htmlmin', 'concat', 'uglify', 'cssmin']);
     grunt.registerTask('default', ['build', 'watch']);
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 };
